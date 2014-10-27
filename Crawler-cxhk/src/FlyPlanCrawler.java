c9e5196f02b7983aed2255919018d11239fa81ee
luoalan <510579050@qq.com>
2014-10-27 13:24:17 +0800

    Signed-off-by: luoalan <510579050@qq.com>

--- Crawler-cxhk/src/FlyPlanCrawler.java
+++ Crawler-cxhk/src/FlyPlanCrawler.java
@@ -4,6 +4,8 @@ import cn.edu.hfut.dmic.webcollector.crawler.BreadthCrawler;
 import cn.edu.hfut.dmic.webcollector.model.Page;
 import cn.edu.hfut.dmic.webcollector.util.Config;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Timer;
 
 import org.jsoup.select.Elements; 
@@ -18,7 +20,11 @@ public class FlyPlanCrawler extends BreadthCrawler{
     public void visit(Page page) {
   	          
     	System.out.println("正在抽取"+page.getUrl());      
-        Elements contents=page.getDoc().select("tr[title=点击查询]");                                             
+        Elements contents=page.getDoc().select("tr[title=点击查询]");  
+        
+        Date now = new Date();
+	SimpleDateFormat dateFormat = new SimpleDateFormat(
+		"yyyy-MM-dd");
 		
     	
             for (int i=0;i<contents.size();i++){
@@ -29,9 +35,9 @@ public class FlyPlanCrawler extends BreadthCrawler{
             	/*获取tr下面的Td集合*/
             	Elements tdcontents = contents.get(i).select("td[style=text-align: center;]");           	           	
             	
-            	String sql2="insert into cxhkFlyPlan " +
-                  		"(OriCity,OriCode,OriTime,DestCity,DestCode,DestTime,Flynumber,DayOWeek,BegPlanDate,EndPlanDate) " +
-                  		"values(?,?,?,?,?,?,?,?,?,?)";
+            	String sql1="insert into cxhkFlyPlan " +
+                  		"(OriCity,OriCode,OriTime,DestCity,DestCode,DestTime,Flynumber,DayOWeek,BegPlanDate,EndPlanDate,CreateDate) " +
+                  		"values(?,?,?,?,?,?,?,?,?,?,?)";
 
                  Object[] obj = new Object[]{tdcontents.get(0).text(),
                 		 Citycodelist[0], 
@@ -42,16 +48,29 @@ public class FlyPlanCrawler extends BreadthCrawler{
                 		 tdcontents.get(4).text(),
                 		 tdcontents.get(5).text(),
                 		 tdcontents.get(6).text(),
-                		 tdcontents.get(7).text()};
+                		 tdcontents.get(7).text(),
+                		 dateFormat.format(now)};
                  
-                 DBHelper.executeNonQuery(sql2,obj);
-
-                 System.out.println(tdcontents.get(0).text() +"到" + tdcontents.get(2).text() +"的航班"+tdcontents.get(4).text() + "插入成功！");                  
+                 DBHelper.executeNonQuery(sql1,obj);                                  
 					      	
             }
+            
+            System.out.println("爬取信息成功！"); 
+            
+            /*判斷今天的數據是否存在,如果存在則刪除之前的所有數據*/
+            
+            String sql2="Select * From cxhkFlyPlan Where CreateDate=?";
+
+            Object[] objExit = new Object[]{dateFormat.format(now)};
+
+            if(DBHelper.isExist(sql2,objExit)){
+        	String sql3="Delete From cxhkFlyPlan Where CreateDate not in(?)";
+        	Object[] objDelete = new Object[]{dateFormat.format(now)};
+        	DBHelper.executeNonQuery(sql3, objDelete);
+        	System.out.println("刪除除今天之外的數據成功！");         	
+            }
+            
           }
- 
-       // }
     
     /*failed函数定制访问访问失败的动作
      * 定义失败后重试i次
